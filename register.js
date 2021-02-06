ed09598b2823d9eb689d0a5fb3b95452ab5eb304
daniellyu33 <daniellyu33@gmail.com>
2021-02-06 15:56:34 -0500

    register

---
+++ register.js
@@ -0,0 +1,7 @@
+const realFilBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-file");
+const customTxt = document.getElementById("custom-text");
+
+custombtn.addEventListener("click", function(){
+    realFilBtn.click();
+}); 
